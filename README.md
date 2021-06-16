807fd9facbb32b5a1dbdb53c081e4d42b7eee0d3
WahidinAji <a17wahidin@gmail.com>
2021-06-16 15:51:06 +0700

    add hapi framework and config server.js

--- README.md
+++ README.md
@@ -2,3 +2,7 @@
  - npm install
 ## running
  - npm run start-dev
+
+ 1. Using Hapi framework
+ 2. using ESLint for code-style
+ 3. using nodemon for run-watch
--- src/server.js
+++ src/server.js
@@ -1,1 +1,19 @@
-console.log('test');
+const Hapi = require('@hapi/hapi');
+
+const init = async () => {
+  const server = Hapi.Server({
+    port: 5000,
+    host: process.env.NODE_ENV !== 'production' ? 'localhost' : '0.0.0.0',
+    routes: {
+      cors: {
+        origin: ['*'],
+      },
+    },
+  });
+
+  server.route();
+  await server.start();
+  console.log(`Server running on ${server.info.uri}`);
+};
+
+init();
